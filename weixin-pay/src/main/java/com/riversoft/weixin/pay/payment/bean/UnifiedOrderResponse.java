d7335d45db687d124890fcc83b30af084470880a
wwy <835802084@qq.com>
2018-03-06 15:58:23 +0800

    统一下单接口返回实体增加mwebUrl(h5支付有效)

--- weixin-pay/src/main/java/com/riversoft/weixin/pay/payment/bean/UnifiedOrderResponse.java
+++ weixin-pay/src/main/java/com/riversoft/weixin/pay/payment/bean/UnifiedOrderResponse.java
@@ -7,51 +7,66 @@ import com.riversoft.weixin.pay.base.BaseResponse;
  * @borball on 5/15/2016.
  */
 public class UnifiedOrderResponse extends BaseResponse {
+	
+	@JsonProperty("device_info")
+	private String deviceInfo;
+	
+	@JsonProperty("trade_type")
+	private String tradeType;
+	
+	@JsonProperty("prepay_id")
+	private String prepayId;
+	
+	/**
+	 * trade_type为NATIVE时有返回，用于生成二维码，展示给用户进行扫码支付
+	 */
+	@JsonProperty("code_url")
+	private String codeUrl;
+	
+	/***
+	 * trade_type为MWEB时候返回 <br/>
+	 * mweb_url为拉起微信支付收银台的中间页面，可通过访问该url来拉起微信客户端，完成支付,mweb_url的有效期为5分钟。
+	 */
+	@JsonProperty("mweb_url")
+	private String mwebUrl;
+	
+	public String getDeviceInfo() {
+		return deviceInfo;
+	}
+	
+	public void setDeviceInfo(String deviceInfo) {
+		this.deviceInfo = deviceInfo;
+	}
+	
+	public String getTradeType() {
+		return tradeType;
+	}
+	
+	public void setTradeType(String tradeType) {
+		this.tradeType = tradeType;
+	}
+	
+	public String getPrepayId() {
+		return prepayId;
+	}
+	
+	public void setPrepayId(String prepayId) {
+		this.prepayId = prepayId;
+	}
+	
+	public String getCodeUrl() {
+		return codeUrl;
+	}
+	
+	public void setCodeUrl(String codeUrl) {
+		this.codeUrl = codeUrl;
+	}
 
-    @JsonProperty("device_info")
-    private String deviceInfo;
+	public String getMwebUrl() {
+		return mwebUrl;
+	}
 
-    @JsonProperty("trade_type")
-    private String tradeType;
-
-    @JsonProperty("prepay_id")
-    private String prepayId;
-
-    /**
-     * trade_type为NATIVE时有返回，用于生成二维码，展示给用户进行扫码支付
-     */
-    @JsonProperty("code_url")
-    private String codeUrl;
-
-    public String getDeviceInfo() {
-        return deviceInfo;
-    }
-
-    public void setDeviceInfo(String deviceInfo) {
-        this.deviceInfo = deviceInfo;
-    }
-
-    public String getTradeType() {
-        return tradeType;
-    }
-
-    public void setTradeType(String tradeType) {
-        this.tradeType = tradeType;
-    }
-
-    public String getPrepayId() {
-        return prepayId;
-    }
-
-    public void setPrepayId(String prepayId) {
-        this.prepayId = prepayId;
-    }
-
-    public String getCodeUrl() {
-        return codeUrl;
-    }
-
-    public void setCodeUrl(String codeUrl) {
-        this.codeUrl = codeUrl;
-    }
+	public void setMwebUrl(String mwebUrl) {
+		this.mwebUrl = mwebUrl;
+	}
 }
